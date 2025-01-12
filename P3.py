d2088e3948b7159a3c30acb92f0a9c11e8e6daad
RuanOliveira <ruanoliveiragf@gmail.com>
2025-01-12 16:31:01 -0300

    Updating code with compression rate

--- P3.py
+++ P3.py
@@ -1,9 +1,20 @@
-dict = {'carro': '%', 'acidente': '$', 'senhor': '&', 'do': '#'} #dictionary with for keyword enconding
+dict = {'carro': '%', 'acidente': '$', 'senhor': '&', 'do': '#'} #dictionary with keyword encoding
 
-text = input().split() #input text
+text = input() #input text
+
+ori_size = len(text) #saving the original text size
+
+text = text.split() #separating the text into words
 
 for i in range(len(text)): #starting a loop to go through the text
-    if text[i] in dict: #checking if the word is on the dictionary
-        text[i] = dict[text[i]] #replacing the word by the symbol
+    if text[i] in dict: #checking if the word is in the dictionary
+        text[i] = dict[text[i]] #replacing the word with the symbol
 
-print(*text) #printing the new text
+text = ' '.join(text) #turning the text into a string again
+
+new_size = len(text) #saving the new text size
+
+com_rate = new_size/ori_size #calculating the compression rate as a percentage
+
+print(text)
+print('Taxa de compressão:{:.2f}%'.format(com_rate)) #printing the new text with the compression rate)
